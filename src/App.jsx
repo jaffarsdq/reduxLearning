6ae55529a222b99b7d631ed9e5441fe02c2c7344
Jaffar sadhiq <jaffarsdq007@gmail.com>
2024-01-08 02:01:56 +0530

    added reducer functions and dispatched it

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useDispatch, useSelector } from 'react-redux';
 import CartContainer from './Components/CartContainer';
 import Navbar from './Components/Navbar';
+import { useEffect } from 'react';
+import { calculateTotals } from './Redux/Slices/CartSlice';
 
 function App() {
+    const { cartItems } = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(calculateTotals());
+    }, [cartItems]);
     return (
         <main>
             <Navbar />
--- src/Redux/Slices/CartSlice.js
+++ src/Redux/Slices/CartSlice.js
@@ -13,9 +13,39 @@ const CartSlice = createSlice({
         clearCart: (state) => {
             state.cartItems = [];
         },
+        removeItem: (state, action) => {
+            const itemId = action.payload;
+            state.cartItems = state.cartItems.filter(
+                (item) => item.id != itemId
+            );
+        },
+        increase: (state, { payload }) => {
+            const cartItem = state.cartItems.find(
+                (item) => item.id === payload
+            );
+            console.log(cartItem);
+            cartItem.amount = cartItem.amount + 1;
+        },
+        decrease: (state, { payload }) => {
+            const cartItem = state.cartItems.find(
+                (item) => item.id === payload
+            );
+            cartItem.amount = cartItem.amount > 0 ? cartItem.amount - 1 : 0;
+        },
+        calculateTotals: (state) => {
+            let amount = 0;
+            let total = 0;
+            state.cartItems.forEach((item) => {
+                amount += item.amount;
+                total += item.amount * item.price;
+            });
+            state.amount = amount;
+            state.total = total;
+        },
     },
 });
 
-export const { clearCart } = CartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+    CartSlice.actions;
 
 export default CartSlice.reducer;
