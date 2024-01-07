2d51494abd9bc11b9ef2f927b45a9bad55b9adf1
Jaffar sadhiq <jaffarsdq007@gmail.com>
2024-01-07 23:12:01 +0530

    added cartContainer and CartItem Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import CartContainer from './Components/CartContainer';
 import Navbar from './Components/Navbar';
 
 function App() {
     return (
         <main>
             <Navbar />
+            <CartContainer />
         </main>
     );
 }
